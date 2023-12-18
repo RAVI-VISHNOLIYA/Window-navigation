9b79bac43cea574c1aa6726d70aff8039da1ebbb
RAVI-VISHNOLIYA <vishnoliya.ravi62@gmail.com>
2023-12-19 01:18:54 +0530

    navigation bar responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const menuItems = [
+    "Home",
+    "About",
+    "Services",
+    "Portfolio",
+    "Contact",
+    "Blog",
+    // Add more items as needed
+  ];
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [visibleItems, setVisibleItems] = useState([]);
+  const [hiddenItems, setHiddenItems] = useState([]);
+  const [isMoreVisible, setIsMoreVisible] = useState(true);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    updateMenuItems();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowWidth]);
+
+  const updateMenuItems = () => {
+    const maxVisibleItems = calculateMaxVisibleItems();
+    setVisibleItems(menuItems.slice(0, maxVisibleItems));
+    setHiddenItems(menuItems.slice(maxVisibleItems));
+  };
+
+  const calculateMaxVisibleItems = () => {
+    const breakpoint = 768;
+    return windowWidth < breakpoint ? 2 : menuItems.length;
+  };
+
+  const renderMenuItems = (items) => {
+    return items.map((item, index) => <li key={index}>{item}</li>);
+  };
+
+  const toggleMoreVisibility = () => {
+    setIsMoreVisible(!isMoreVisible);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul className="visible-menu">{renderMenuItems(visibleItems)}</ul>
+        {hiddenItems.length > 0 && (
+          <div>
+            <button onClick={toggleMoreVisibility}>
+              {isMoreVisible ? "Hide More" : "Show More"}
+            </button>
+            {isMoreVisible && (
+              <ul className="hidden-menu">
+                <li>More</li>
+                {renderMenuItems(hiddenItems)}
+              </ul>
+            )}
+          </div>
+        )}
+      </nav>
     </div>
   );
-}
+};
 
 export default App;
